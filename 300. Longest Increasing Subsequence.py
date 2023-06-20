e1f5ee1a8c335325cf731b7232b925300e0fcc5b
tangqianle <qtang@ucsb.edu>
2023-06-20 16:44:16 -0500

    update

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -4,9 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
+        # loop through all possible index in the string as endpoint, and the resulkt of the current end point value will be the max
+        # value before it +1 or itself(ofcourse you have to be larger than the point before you)
         if not nums:
             return 0
+
         dp = []
         for i in range(len(nums)):
             dp.append(1)
