e5e32ab652945d3ceea35b3e94f52965483eaf60
tangqianle <qtang@ucsb.edu>
2023-06-20 16:23:42 -0500

    update

---
+++ 516. Longest Palindromic Subsequence.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        matrix = [[0]*n for i in range(n)]
+        for i in range(n):
+            matrix[i][i] = 1
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i+1,n):
+                if s[i] == s[j]:
+                    matrix[i][j] = matrix[i+1][j-1] + 2
+                else:
+                    matrix[i][j] = max(matrix[i+1][j],matrix[i][j-1])
+        return matrix[0][n - 1]
