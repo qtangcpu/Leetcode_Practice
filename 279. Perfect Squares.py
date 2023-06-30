3e1e46b7e2304c201ce2b7cd650962d770440446
tangqianle <qtang@ucsb.edu>
2023-06-30 15:47:19 -0500

    update

--- 279. Perfect Squares.py
+++ 279. Perfect Squares.py
@@ -6,11 +6,11 @@ class Solution(object):
         """
         # create a list of perfect squares that are less than n
         nums = [i ** 2 for i in range(1, n + 1) if i ** 2 <= n]
-        dp = [float('inf')] * (n + 2)
+        dp = [float('inf')] * (n + 1)
         dp[0] = 0
 
         for i in range(1, n + 1):
             for num in nums:
                 if i >= num:
                     dp[i] = min(dp[i], dp[i - num] + 1)
-        return dp[n]
+        return dp[-1]
