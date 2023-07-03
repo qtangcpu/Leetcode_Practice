e0e96c4980fc057b44fe2162b5c285097c595cdd
tangqianle <qtang@ucsb.edu>
2023-07-03 15:36:21 -0500

    update

---
+++ 518. Coin Change II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        # dp: 要用外循环循环硬币，这样可以保证不会有重复出现
+
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+        return dp[amount]
