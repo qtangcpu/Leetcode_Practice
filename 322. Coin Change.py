a34963c45535aa282054623bf746ca693d9dbe60
tangqianle <qtang@ucsb.edu>
2023-07-05 16:19:26 -0500

    update

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -7,8 +7,7 @@ class Solution(object):
         """
         dp = [float('inf')] * (amount + 1)
         dp[0] = 0
-
         for coin in coins:
             for x in range(coin, amount + 1):
                 dp[x] = min(dp[x], dp[x - coin] + 1)
-        return dp[amount] if dp[amount] != float('inf') else -1
+        return dp[-1] if dp[-1] != float('inf') else -1
